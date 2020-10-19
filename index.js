1eb089554464e55d144b838cc0e4b1f9083781f1
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-10-19 16:28:26 +0300

    Import the js file where the works is rendered

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Windbnb from './Windbnb';
 
 
-ReactDOM.render(<h1>Hello</h1>, document.getElementById("root"));
+ReactDOM.render(<Windbnb />, document.getElementById("root"));
