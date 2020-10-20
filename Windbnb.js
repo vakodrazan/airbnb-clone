ca747f0137aa7c2bc713efc16489e21252810d4e
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-10-20 11:22:45 +0300

    Move the content class in here and wrap the element that is mapping through the list inside of it to get the layout

--- Windbnb.js
+++ Windbnb.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import data from './stays.json';
 import WindbnbList from './WindbnbList';
+import SearchForm from './SearchForm';
 
 export default function Windbnb() {
     return (
-        <>
-            {
-                data.map(stay => {
-                    return (
-                        <WindbnbList key={stay.rating} stay={stay} />
-                    )
-                })
-            }
+        <> 
+            <SearchForm />
+            <div className="content">
+                {
+                    data.map(stay => {
+                        return (
+                            <WindbnbList key={stay.rating} stay={stay} />
+                        )
+                    })
+                }
+            </div>
         </>
     )
 }
