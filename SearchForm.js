59514b0242ce8508a6eff4eacee50b82244a4644
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-10-20 11:37:01 +0300

    This file is for the form component

---
+++ SearchForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export default function SearchForm() {
+    return (
+        <form>
+            <fieldset>
+                <label>Location</label>
+                <input type="text" />
+            </fieldset>
+        </form>
+    )
+}
