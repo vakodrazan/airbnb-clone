c72863188e129af6bcec9f9c7d9bec28c98a0b79
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-10-21 11:51:36 +0300

    Add class name with some element to get the style of it

--- components/FormComponent.js
+++ components/FormComponent.js
@@ -3,23 +3,29 @@ import React from 'react';
 
 export default function FormComponent(props) {
     return (
-        <form onSubmit={props.searchStay}>
-            <select 
-                name="location"
-                value={props.location}
-                onChange={props.onChange}
-            >
-                <option value="">-- Where do you want to go? --</option>
-                <option value="helsinki">Helsinki</option>
-                <option value="turku">Turku</option>
-                <option value="vaasa">Vaasa</option>
-                <option value="oulu">Oulu</option>
-            </select>
-            <input 
-                type="number"
-                name="numberOfGuest"
-                placeholder="How many of you will stay there"
-            />
+        <form onSubmit={props.searchStay} className="search-form">
+            <fieldset className="location">
+                <label>Location</label>
+                <select 
+                    name="location"
+                    value={props.location}
+                    onChange={props.onChange}
+                >
+                    <option value="">-- Where do you want to go? --</option>
+                    <option value="helsinki">Helsinki</option>
+                    <option value="turku">Turku</option>
+                    <option value="vaasa">Vaasa</option>
+                    <option value="oulu">Oulu</option>
+                </select>
+            </fieldset>
+            <fieldset className="guest">
+                <label>Guests</label>
+                <input 
+                    type="number"
+                    name="numberOfGuest"
+                    placeholder="How many of you will stay there"
+                />
+            </fieldset>
             <button type="submit">Search</button>
         </form>
     )
